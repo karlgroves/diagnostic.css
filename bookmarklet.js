f10ca8bc61822dada23eca96c560b05c55bd5adf
Karl Groves <karl@simplyaccessible.com>
2013-05-23 08:40:16 -0400

    de-minified the bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,13 @@
-javascript:(function(d,i,l){l=d.getElementById(i);if(l){l.parentNode.removeChild(l);return;}l=d.createElement('link');l.id=i;l.rel='stylesheet';l.type='text/css';l.href='//raw.github.com/karlgroves/diagnostic.css/master/diagnostic.css';d.getElementsByTagName('head')[0].appendChild(l);}(document,'diagnosticCSS'))
+javascript:( function(d, i, l) {
+		l = d.getElementById(i);
+		if (l) {
+			l.parentNode.removeChild(l);
+			return;
+		}
+		l = d.createElement('link');
+		l.id = i;
+		l.rel = 'stylesheet';
+		l.type = 'text/css';
+		l.href = '//raw.github.com/karlgroves/diagnostic.css/master/diagnostic.css';
+		d.getElementsByTagName('head')[0].appendChild(l);
+	}(document, 'diagnosticCSS'))
